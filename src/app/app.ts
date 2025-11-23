213bd225691d3257e612733e42ff98e396b2fc43
ahc-AIG-1976 <angel.herrera391@comunidadunir.net>
2025-11-23 19:34:29 +0100

    Subiendo a GitHub

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Test } from "./test/test";
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Carrusel } from './carrusel/carrusel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Test, Header, Footer, Carrusel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-21-v1');
+  idenet = "Esta Variable esta en el tapiz principal app";
 }
